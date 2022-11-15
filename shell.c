a5aa06d088965c1175f75fe25cb2553d8c66f809
Mgregchi <mgregchi.tk>
2022-11-14 16:28:27 -0800

    Create AUTHORS, shell.c files

---
+++ shell.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+
+/**
+ * main - Shell main entry function
+ *
+ * @argc: Argument count
+ * @argv: Arguments values
+ *
+ * Return: Always 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+printf("Aregument count: %i\n", argc);
+printf("Arguments %s\n", *argv);
+
+return (0);
+}
