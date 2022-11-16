cc8fb98f9b10ac54896eda3d4f73fe518ad1b2b1
Mgregchi <mgregchi.tk>
2022-11-16 14:40:55 -0800

    Complete prlject

--- shell.c
+++ shell.c
@@ -1,20 +1,17 @@
-#include "shell.h"
-
+#include "holberton.h"
 
 /**
- * main - Shell main entry function
- *
- * @argc: Argument count
- * @argv: Arguments values
- *
- * Return: Always 0
- */
-
-
-int main(int argc, char *argv[])
+ * main - func with infinite loop
+ * @ac: No use
+ * @av: No use
+ * Return: loop.
+ **/
+int main(int ac, char **av)
 {
-printf("Aregument count: %i\n", argc);
-printf("Arguments %s\n", *argv);
+	(void)av;
+	(void)ac;
 
-return (0);
+	signal(SIGINT, controlC);
+	prompt();
+	return (0);
 }
